04d952ffbb67f5fec8853f279edda86635936e2e
AntonSD <anton89.ad@gmail.com>
2022-07-26 10:09:24 +0300

    Create example.py

---
+++ example.py
@@ -0,0 +1,33 @@
+size_of_side = float(input())
+number_of_sheets = int(input())
+
+total_size = size_of_side * size_of_side * 6
+sum = 0
+counter = 0
+for sheet in range(number_of_sheets):
+    counter += 1
+    length = float(input())
+    width = float(input())
+    cover_of_sheet = length * width
+    if counter % 3 == 0:
+        if counter % 5 == 0:
+            continue
+        cover = cover_of_sheet * 0.75
+        sum += cover
+        continue
+    elif counter % 5 == 0:
+        continue
+    else:
+        sum += cover_of_sheet
+if total_size > 0:
+    area_covered = (sum / total_size) * 100
+    difference = 100 - area_covered
+if sum > 0:
+    left_cover = (total_size / sum) * 100
+    diff_left_cover = 100 - left_cover
+if total_size <= sum:
+    print("You've covered the gift box!")
+    print(f"{diff_left_cover:.2f}% wrap paper left.")
+else:
+    print("You are out of paper!")
+    print(f"{difference:.2f}% of the box is not covered.")
